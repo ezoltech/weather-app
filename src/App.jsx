4c5264b93cc18f2e3e5ed1a9700c377540e8ddf2
ezoltech <ezramamo13@gmail.com>
2022-11-23 18:39:47 -0800

    adding-weather-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { React, useState } from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  // const url = "https://api.openweathermap.org/data/2.5/weather?lat=8.9806&lon=38.7578&appid=3f4b7a8e539b0b108265533801b479bf";
   return (
     <div className="App">
       <h1>hello world</h1>
