23d3bf81bac93593273ec4eedb1cfb6276b5012f
ezoltech <ezramamo13@gmail.com>
2022-11-23 23:31:06 -0800

    testing project and deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { React, useState } from 'react'
 import './App.css';
 import axios from 'axios'
+import Slide from 'react-reveal/Slide'
+import Bounce from 'react-reveal/Bounce'
 
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
   // eslint-disable-next-line no-template-curly-in-string
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=3f4b7a8e539b0b108265533801b479bf`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=3f4b7a8e539b0b108265533801b479bf`;
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
@@ -16,47 +18,64 @@ function App() {
     }
 
   }
+
   return (
     <div className="app">
       <div className="searchbar">
-        <input
-          type="text"
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter Location'
-        />
+        <Slide left>
+          <input
+            type="text"
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+          />
+        </Slide>
+
       </div>
       <div className="container">
         {/* upper part */}
         <div className="top">
           <div className="location">
-            <p>Addis Ababa</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>60◦C</h1>
+            {data.main ? <h1>{data.main.temp}◦C</h1> : null}
           </div>
           <div className="description">
-            <p>clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+
           </div>
         </div>
         {/* lower part */}
-        <div className="bottom">
-          <div className="feels">
-            <p className='bold'>65◦C</p>
-            <p>Feels Like</p>
+
+        {data.name != undefined &&
+          < div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like}◦C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}</p> : null}
+              <p>Humidty</p>
+            </div>
+            <div className="wind">
+              {data.main ? <p className='bold'>{data.wind.speed} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humidity">
-            <p className='bold'>20</p>
-            <p>Humidty</p>
-          </div>
-          <div className="wind">
-            <p className='bold'>12MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        }
+
+
+
+        <h3>Made by <a href='https://github.com/ezoltech'>Ezolbest</a></h3>
+        <Bounce bottom>
+          <h2>👋 Hi there, you can use this weather app by just search and hit enter from your keyboard</h2>
+        </Bounce>
+
       </div>
-    </div>
+
+    </div >
   );
 }
 
